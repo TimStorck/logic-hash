92bb6de2da76554eec363ff790bfbf9c6ee4fcef
Tim Storck <timstorck0@gmail.com>
2016-04-14 19:22:12 -0400

    (dev): route links for dev-add and dev

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -5,6 +5,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { home } from '../../ui/home/home.js';
 import { layoutDev } from '../../ui/dev-layout/layout-dev.js';
 import { devHome } from '../../ui/dev-home/dev-home.js';
+import { devAdd } from '../../ui/dev-add/dev-add.js';
 
 FlowRouter.route('/', {
     name: 'home',
@@ -17,5 +18,12 @@ FlowRouter.route('/dev/', {
     name: 'dev',
     action: function() {
         BlazeLayout.render("layoutDev", {content: "devHome"});
+    }
+});
+
+FlowRouter.route('/dev/add', {
+    name: 'dev_add',
+    action: function() {
+        BlazeLayout.render("layoutDev", {content: "devAdd"});
     }
 });
--- imports/ui/dev-layout/layout-dev.js
+++ imports/ui/dev-layout/layout-dev.js
@@ -3,11 +3,11 @@ import { Template } from 'meteor/templating';
 import './layout-dev.html';
 
 Template.layoutDev.events ({
-  "click #btn-test-temp": function() {
-    
-    console.log("btn-test-flow clicked");
-    
-    console.log(Template);
+  "click #btn-tbl": function() {
+    FlowRouter.go("dev");
+  },
+  "click #btn-add": function() {
+    FlowRouter.go("dev_add");
   },
   "click #btn-add-doc": function() {
     console.log("btn-add-doc clicked");
