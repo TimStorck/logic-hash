05740fa236e1a0f593af7128bccd28b0c8baa0ed
Tim Storck <timstorck0@gmail.com>
2016-04-29 07:53:24 -0400

    (sandbox): draw line works
    called from event on arbitrary points. started trying to fix console exception at startup

--- imports/ui/dev-layout/dev-layout.js
+++ imports/ui/dev-layout/dev-layout.js
@@ -28,5 +28,19 @@ Template.devLayout.events ({
   "click #btn-tst": function() {
     console.log("btn-tst clicked");
     FlowRouter.go("dev_test");
+  },
+  "click #btn-cvs": function() {
+    console.log("btn-cvs clicked");
+    FlowRouter.go("sandbox");
+  },
+  "click #btn-line": function() {
+    const c = document.getElementById("canvas");
+    const ctx = c.getContext("2d");
+    const points = [[10,10],[20,20],[10,20],[30,20]];
+    for (var i = 1; i < points.length; i++) {
+      ctx.moveTo(points[i-1][0],points[i-1][1]);
+      ctx.lineTo(points[i][0],points[i][1]);
+      ctx.stroke();
+    }
   }
 });
--- imports/ui/dev-sandbox/dev-sandbox.js
+++ imports/ui/dev-sandbox/dev-sandbox.js
@@ -1,3 +1,17 @@
 import { Template } from 'meteor/templating';
 
 import './dev-sandbox.html';
+
+
+Template.sandbox.onCreated( function() {
+  // const cs = document.getElementById("canvas");
+  // const ctx = c.getContext("2d");
+});
+
+// // function draw() {
+
+// // }
+
+// Template.sandbox.events({
+
+// });
