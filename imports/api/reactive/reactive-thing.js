6efdf17bdc09325cc87df892c37cc44b091670e8
Tim Storck <timstorck0@gmail.com>
2016-05-02 16:02:24 -0400

    (reactive): remove unneeded import, make thing() return value

--- imports/api/reactive/reactive-thing.js
+++ imports/api/reactive/reactive-thing.js
@@ -1,16 +1,8 @@
 import { Meteor } from 'meteor/meteor';
-import { FlowRouter } from 'meteor/kadira:flow-router';
-
 import {Posts} from '../collections/posts.js';
-
-if (Meteor.isClient) {
-  if (FlowRouter.subsReady()) {
-    Meteor.subscribe('posts');  
-  }
-}
 
 export function thing() {
   console.log("thing called");
-    Meteor.subscribe('posts');  
-  console.log(Posts.find().count());
+  Meteor.subscribe('posts');  
+  return Posts.find().count();
 }
