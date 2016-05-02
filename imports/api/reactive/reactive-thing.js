038d5c2a3d13dac2cd29a6a9694e42bb5a7a09e6
Tim Storck <timstorck0@gmail.com>
2016-05-02 15:20:41 -0400

    (reactive): import posts and subscribe in reactive-thing.js, even though it doesn't work

--- imports/api/reactive/reactive-thing.js
+++ imports/api/reactive/reactive-thing.js
@@ -1,3 +1,14 @@
+import { Meteor } from 'meteor/meteor';
+
+import {Posts} from '../collections/posts.js';
+
+if (Meteor.isClient) {
+  if (FlowRouter.subsReady()) {
+    Meteor.subscribe('posts');  
+  }
+}
+
 export function thing() {
   console.log("thing called");
+  console.log(Posts.find().count());
 }
