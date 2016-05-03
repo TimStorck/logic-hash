7ec6c705e792fba83011d37c79629fce94e2a1e9
Tim Storck <timstorck0@gmail.com>
2016-05-03 19:06:33 -0400

    (signup): set up form elements with js event and logic

--- imports/ui/signup/signup.js
+++ imports/ui/signup/signup.js
@@ -1,3 +1,21 @@
 import { Template } from 'meteor/templating';
 
 import './signup.html';
+
+Template.signUp.events({
+  'submit #signup' : function(event, template) {
+
+    event.preventDefault();
+
+    const email = template.find('#email').value;
+    const username = template.find('#username').value;
+    const password = template.find('#password').value;
+    const confirmPassword = template.find('#confirmPassword').value;
+
+    if (password === confirmPassword) {
+      
+    } else {
+      document.getElementById("msg").innerHTML = "Passwords don't match";
+    }
+  }
+});
