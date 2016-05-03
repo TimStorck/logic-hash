b9d6cdcf1f58ed2ed1ecfdf35d7c353663f5da26
Tim Storck <timstorck0@gmail.com>
2016-05-03 14:48:49 -0400

    (posts db): update dummy data

--- imports/api/data/dummy-posts.js
+++ imports/api/data/dummy-posts.js
@@ -14,12 +14,48 @@ import '../collections/methods.js';
 export function insertPosts() {
   const postsArray = [
     {
-      author: "joe",
-      content: "waza"
+      author: "Joe",
+      content: "I think that global warming is the most important issue facing civilization."
     },
     {
-      author: "ben",
-      content: "yes"
+      author: "Ben",
+      content: "Citizens should be allowed to vote on legislation from their computer."
+    },
+    {
+      author: "Jennifer",
+      content: "Nasa should receive 10x more funding in order to protect Earth from asteroids."
+    },
+    {
+      author: "Melissa",
+      content: "Rock climbing should be in the Olympics."
+    },
+    {
+      author: "George",
+      content: "Classical music history should be taught in public schools."
+    },
+    {
+      author: "Ben From CT",
+      content: "Congress needs to do something about the use of anti-biotics on livestock."
+    },
+    {
+      author: "Joanne",
+      content: "Internet piracy should be policed by a Federal agency."
+    },
+    {
+      author: "Bethany",
+      content: "Internet piracy should be policed by a Federal agency."
+    },
+    {
+      author: "Fred",
+      content: "Wikipedia is as reliable as anything else."
+    },
+    {
+      author: "Zeb",
+      content: "Dinosaur fossils could be a trick."
+    },
+    {
+      author: "Debby",
+      content: "Old trees should be protected by local governments."
     }
   ];
   
