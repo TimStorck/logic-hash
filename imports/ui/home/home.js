88fb326bd0ff1da1058f4b3188dba6f7703d4813
Tim Storck <timstorck0@gmail.com>
2016-05-03 11:25:15 -0400

    (home): put db queries in helpers
    just find() for now

--- imports/ui/home/home.js
+++ imports/ui/home/home.js
@@ -1,16 +1,17 @@
 import { Template } from 'meteor/templating';
+import {Posts} from '../../api/collections/posts.js';
 
 import './home.html';
 
+Template.home.onCreated(function() {
+  Meteor.subscribe('posts');
+});
+
 Template.home.helpers({
-  recentlyAdded: [
-    { text: 'added 1' },
-    { text: 'added 2' },
-    { text: 'added 3' },
-  ],
-  recentlyActive: [
-    { text: 'active 1' },
-    { text: 'active 2' },
-    { text: 'active 3' },
-  ]
+  recentlyAdded() {
+    return Posts.find();
+  },
+  recentlyActive() {
+    return Posts.find();
+  }
 });
