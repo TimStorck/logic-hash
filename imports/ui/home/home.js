e9652324fa0d70fe93b883f9c67ac8f817e444ac
Tim Storck <timstorck0@gmail.com>
2016-05-03 11:08:09 -0400

    (home): add helpers, mustache tags, listitem template
    working with dummy array

--- imports/ui/home/home.js
+++ imports/ui/home/home.js
@@ -1,3 +1,16 @@
 import { Template } from 'meteor/templating';
 
 import './home.html';
+
+Template.home.helpers({
+  recentlyAdded: [
+    { text: 'added 1' },
+    { text: 'added 2' },
+    { text: 'added 3' },
+  ],
+  recentlyActive: [
+    { text: 'active 1' },
+    { text: 'active 2' },
+    { text: 'active 3' },
+  ]
+});
