075cd251ff5f20560043a3f2c8ad89111dddf42b
Tim Storck <timstorck0@gmail.com>
2016-05-06 02:55:22 -0400

    (debate): rename responseCount to debateTreeChanged

--- imports/api/reactive/reactive.js
+++ imports/api/reactive/reactive.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import {Posts} from '../collections/posts.js';
 
-export function responseCount(elicitor) {
+export function debateTreeChanged(elicitor, wrapper) {
   Meteor.subscribe('posts');  
   return Posts.find({elicitor: elicitor}).count();
 }
--- imports/ui/debate/debate.js
+++ imports/ui/debate/debate.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
 import {Posts} from '../../api/collections/posts.js';
 import { Meteor } from 'meteor/meteor';
-import {responseCount} from '../../api/reactive/reactive.js';
+import {debateTreeChanged} from '../../api/reactive/reactive.js';
 
 import './debate.html';
 
@@ -9,6 +9,6 @@ Template.debate.onCreated(function() {
   Meteor.subscribe('posts');
 
   const handle = this.autorun(function () {
-    console.log(responseCount(FlowRouter.getParam("mId")) + " hi"); 
+    debateTreeChanged(FlowRouter.getParam("mId")); 
   });
 });
