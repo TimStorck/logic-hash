c9f586ca948f7112fd368feb129336ef973d1445
Tim Storck <timstorck0@gmail.com>
2016-05-06 11:55:43 -0400

    (debate): get dynamic text-box rendering on canvas to work
    on debate page, motion content from db, specified by router parameter, now rendered in styled div, placed by js to coordinates over canvas

--- imports/api/reactive/reactive.js
+++ imports/api/reactive/reactive.js
@@ -6,8 +6,13 @@ export function debateTreeChanged(motion, bucket, canCtx) {
   Meteor.subscribe('posts');  
 
   try {
-      let content = Posts.findOne({"_id": motion}).content;
-      bucket.innerHTML = content;
+    let content = Posts.findOne({"_id": motion}).content;
+    var newElem = document.createElement("div");
+    newElem.setAttribute("class", "motion");
+    newElem.innerHTML = content;
+    newElem.style.top = "250px";
+    newElem.style.left = "450px";
+    bucket.appendChild(newElem);
   } catch(e) {
 
   }
