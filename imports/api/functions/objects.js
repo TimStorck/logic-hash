f76ca13a5ef8af99cd2072ec040faaee967dfcb9
Tim Storck <timstorck0@gmail.com>
2016-05-08 03:39:36 -0400

    (funtions): fill in logic for update methods of fsmodel
    edge case section not done yet

--- imports/api/functions/objects.js
+++ imports/api/functions/objects.js
@@ -41,37 +41,61 @@ export function filledSpaceModel() {
   this.updateTopLine = function(topLeft, bottomRight, referredByAdjacent) {
     let topLeftMargin = new Coord(topLeft.x - margin, topLeft.y - margin);
     let topRightMargin = new Coord(bottomRight.x + margin, topLeft.y - margin);
-    if (referredByAdjacent) {
+    if (!referredByAdjacent) {
 
-    } else {
-
+    } 
+    for (let i=0; i < this.topLine.length; i++) {
+      if (topLine[i].x > topLeftMargin.x && topLine[i].x < topRightMargin) {
+        this.topLine = this.topLine.splice(i, 1);
+        i--;
+      }
     }
+    this.topLine.push(topLeftMargin);
+    this.topLine.push(topRightMargin);
   };
   this.updateRightLine = function(topLeft, bottomRight, referredByAdjacent) {
     let topRightMargin = new Coord(bottomRight.x + margin, topLeft.y - margin);
     let bottomRightMargin = new Coord(bottomRight.x + margin, bottomRight.y + margin);
-    if (referredByAdjacent) {
+    if (!referredByAdjacent) {
       
-    } else {
-      
+    } 
+    for (let i=0; i < this.rightLine.length; i++) {
+      if (rightLine[i].y < topRightMargin.x && rightLine[i].y > bottomRightMargin) {
+        this.rightLine = this.rightLine.splice(i, 1);
+        i--;
+      }
     }
+    this.rightLine.push(bottomRightMargin);
+    this.rightLine.push(topRightMargin);
   };
   this.updateBottomLine = function(topLeft, bottomRight, referredByAdjacent) {
     let bottomLeftMargin = new Coord(topLeft.x - margin, bottomRight.y + margin);
     let bottomRightMargin = new Coord(bottomRight.x + margin, bottomRight.y + margin);
-    if (referredByAdjacent) {
+    if (!referredByAdjacent) {
       
-    } else {
-      
+    } 
+    for (let i=0; i < this.bottomLine.length; i++) {
+      if (bottomLine[i].x > bottomLeftMargin.x && bottomLine[i].x < bottomRightMargin) {
+        this.bottomLine = this.bottomLine.splice(i, 1);
+        i--;
+      }
     }
+    this.bottomLine.push(bottomLeftMargin);
+    this.bottomLine.push(bottomRightMargin);
   };
   this.updateLeftLine = function(topLeft, bottomRight, referredByAdjacent) {
     let topLeftMargin = new Coord(topLeft.x - margin, topLeft.y - margin);
     let bottomLeftMargin = new Coord(topLeft.x - margin, bottomRight.y + margin);
-    if (referredByAdjacent) {
+    if (!referredByAdjacent) {
       
-    } else {
-      
+    } 
+    for (let i=0; i < this.leftLine.length; i++) {
+      if (leftLine[i].y < topLeftMargin.x && leftLine[i].y > bottomLeftMargin) {
+        this.leftLine = this.leftLine.splice(i, 1);
+        i--;
+      }
     }
+    this.leftLine.push(topLeftMargin);
+    this.leftLine.push(bottomLeftMargin);
   };
 }
