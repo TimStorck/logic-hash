bbc735816b5a38b1cc9bf96e8f16c32d1f7da4ff
Tim Storck <timstorck0@gmail.com>
2016-05-08 00:00:00 -0400

    (functions): move stuff from try catch in reactive.js that doesnt need to be there

--- imports/api/functions/reactive.js
+++ imports/api/functions/reactive.js
@@ -9,26 +9,32 @@ import { centerOf } from './measurements.js';
 export function debateTreeChanged(motionId, bucket, canvas) {
   Meteor.subscribe('posts');  
 
-  try {
-    let center = centerOf(new Coord(canvas.width, canvas.height));
+  let center = centerOf(new Coord(canvas.width, canvas.height));
 
-    let motion = new PostOb(
+  let motion;
+
+  try {
+    motion = new PostOb(
       Posts.findOne({"_id": motionId}).author, 
       Posts.findOne({"_id": motionId}).content,
       Posts.find({"elicitor": motionId}).count()
     );
+  } catch(e) {
+    console.log("Exception thrown loading motion data");
+  }
 
+  if (typeof motion != "undefined") {
     drawTextBox(motion, bucket, center);
 
-    let responseArray = Posts.find({"elicitor": motionId}).fetch();
+    let fetchArray = Posts.find({"elicitor": motionId}).fetch();
+    // let responseArray = [];
 
-    for (let i = 0; i < responseArray.length; i++) {
-      console.log(responseArray[i].content);
-    }
-
-  } catch(e) {
-    /*
-    this try catch put in place because of exception thrown by "Posts.findOne({"_id": motion}).content" when page loading at meteor app startup. if debate page navigated to from landing page it would load fine, and if returned to by the url it would load fine
-    */
+    // for (let i = 0; i < responseArray.length; i++) {
+    //   responseArray.push(new PostOb(
+    //     fetchArray[i].author,
+    //     fetchArray[i].content,
+    //     fetchArray[i]._id
+    //   ));
+    // }
   }
 }
