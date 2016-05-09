dbddaa94edac1ab6060c8dfb2e96376468a34547
Tim Storck <timstorck0@gmail.com>
2016-05-09 17:23:55 -0400

    (debate): set up event listener for flag button that gets id from parent div

--- imports/ui/debate/debate.js
+++ imports/ui/debate/debate.js
@@ -18,7 +18,10 @@ Template.debate.onRendered(function() {
 });
 
 Template.debate.events({
-  'click .post': function(event) {
-    console.log("click" + event.target.id);
+  // 'click .post': function(event) {
+  //   console.log("click post " + event.target.id);
+  // },
+  'click .flagBox': function(event) {
+    console.log("click flagbox" + event.currentTarget.parentNode.id);
   }
 });
