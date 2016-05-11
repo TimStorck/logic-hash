fe784239ec7616382e152685af3b105a0657a04b
Tim Storck <timstorck0@gmail.com>
2016-05-11 16:51:42 -0400

    (drawing.js): consolidate and organize code into smaller functions

--- imports/api/functions/drawing.js
+++ imports/api/functions/drawing.js
@@ -9,29 +9,9 @@ import { topMost } from './measurements.js';
 import { bottomMost } from './measurements.js';
 
 export function drawMotionTextBox(post, butcket, centerPos, fSModel) {
-  let newElem = document.createElement("div");
-    newElem.setAttribute("class", "post motion");
-    newElem.setAttribute("id", post._id);
-      let flagDiv = document.createElement("div");
-      flagDiv.setAttribute("class", "flagBox");
-      flagDiv.innerHTML = "Flag";
-      newElem.appendChild(flagDiv);
-
-      let authortDiv = document.createElement("div");
-      authortDiv.setAttribute("class", "author");
-      authortDiv.innerHTML = post.author;
-      newElem.appendChild(authortDiv);
-
-      let contentDiv = document.createElement("div");
-      contentDiv.setAttribute("class", "content");
-      contentDiv.innerHTML = post.content;
-      newElem.appendChild(contentDiv);
-      
-    newElem.style.width = widthFromChars(post.content.length);
-
-  bucket.appendChild(newElem);
-  
+  let newElem = createTextBoxElement(post, bucket);
   let dimens = dimensOf(newElem);
+  
   let topLeftPos = centerPos.minus(centerOf(dimens));
   newElem.style.top = topLeftPos.yPx();
   newElem.style.left = topLeftPos.xPx();
@@ -40,58 +20,22 @@ export function drawMotionTextBox(post, butcket, centerPos, fSModel) {
 }
 
 export function drawResponseTextBox(post, bucket, fSModel, sideOscillator) {
-  let newElem = document.createElement("div");
-    newElem.setAttribute("class", "post");
-    newElem.setAttribute("id", post._id);
-      let flagDiv = document.createElement("div");
-      flagDiv.setAttribute("class", "flagBox");
-      flagDiv.innerHTML = "Flag";
-      newElem.appendChild(flagDiv);
-      
-      let authortDiv = document.createElement("div");
-      authortDiv.setAttribute("class", "author");
-      authortDiv.innerHTML = post.author;
-      newElem.appendChild(authortDiv);
-
-      let contentDiv = document.createElement("div");
-      contentDiv.setAttribute("class", "content");
-      contentDiv.innerHTML = post.content;
-      newElem.appendChild(contentDiv);
-
-    newElem.style.width = widthFromChars(post.content.length);
-
-  bucket.appendChild(newElem);
-
+  let newElem = createTextBoxElement(post, bucket);
   let dimens = dimensOf(newElem);
-  let centerPos;
-  let topLeftPos;
-  switch(sideOscillator) {
-    case 0:
-      centerPos = fSModel.findSpotLeft(dimens);
-      topLeftPos = centerPos.minus(centerOf(dimens));
-      fSModel.updateLeftLine(topLeftPos, topLeftPos.plus(dimens), false);
-      break;
-    case 1:
-      centerPos = fSModel.findSpotRight(dimens);
-      topLeftPos = centerPos.minus(centerOf(dimens));
-      fSModel.updateRightLine(topLeftPos, topLeftPos.plus(dimens), false);
-      break;
-    case 2:
-      centerPos = fSModel.findSpotTop(dimens);
-      topLeftPos = centerPos.minus(centerOf(dimens));
-      fSModel.updateTopLine(topLeftPos, topLeftPos.plus(dimens), false);
-      break;
-    case 3:
-      centerPos = fSModel.findSpotBottom(dimens);
-      topLeftPos = centerPos.minus(centerOf(dimens));
-      fSModel.updateBottomLine(topLeftPos, topLeftPos.plus(dimens), false);
-  }
+
+  let centerPos = getPosition(fSModel, sideOscillator, dimens);
+  let topLeftPos = centerPos.minus(centerOf(dimens))
   newElem.style.top = topLeftPos.yPx();
   newElem.style.left = topLeftPos.xPx();
 
+  updateFSM(topLeftPos, dimens, fSModel, sideOscillator);
   return centerPos;
 }
 
+
+/*
+  for development, to view filled space model boundaries
+*/
 export function drawFSModel(fSModel, canvas) {
   let canCtx = canvas.getContext("2d");
 
@@ -109,10 +53,6 @@ export function drawFSModel(fSModel, canvas) {
   }
 }
 
-/*
-  this function is for development use, to check out the filled space model boundaries
-  (called from reactive.js)
-*/
 function drawLine(platform, canCtx) {
   canCtx.beginPath();
   canCtx.strokeStyle = "red";
@@ -129,4 +69,68 @@ export function drawRadial(motionCenter, responseCenter, canCtx) {
   canCtx.lineTo(responseCenter.x, responseCenter.y);
   canCtx.stroke();
   canCtx.closePath();
+}
+
+function createTextBoxElement(post, bucket) {
+  let newElem = document.createElement("div");
+    newElem.setAttribute("class", "post");
+    newElem.setAttribute("id", post._id);
+      let flagDiv = document.createElement("div");
+      flagDiv.setAttribute("class", "flagBox");
+      flagDiv.innerHTML = "Flag";
+      newElem.appendChild(flagDiv);
+
+      let authortDiv = document.createElement("div");
+      authortDiv.setAttribute("class", "author");
+      authortDiv.innerHTML = post.author;
+      newElem.appendChild(authortDiv);
+
+      let contentDiv = document.createElement("div");
+      contentDiv.setAttribute("class", "content");
+      contentDiv.innerHTML = post.content;
+      newElem.appendChild(contentDiv);
+      
+    newElem.style.width = widthFromChars(post.content.length);
+
+  bucket.appendChild(newElem);
+
+  return newElem;
+}
+
+function getPosition(fSModel, sideOscillator, dimens) {
+  switch(sideOscillator) {
+    case 0:
+      return fSModel.findSpotLeft(dimens);
+      break;
+    case 1:
+      return fSModel.findSpotRight(dimens);
+      break;
+    case 2:
+      return fSModel.findSpotTop(dimens);
+      break;
+    case 3:
+      return fSModel.findSpotBottom(dimens);
+      break;
+    default:
+      console.log("sideOscillator invalid");
+  }
+}
+
+function updateFSM(topLeftPos, dimens, fSModel, sideOscillator) {
+  switch(sideOscillator) {
+    case 0:
+      fSModel.updateLeftLine(topLeftPos, topLeftPos.plus(dimens), false);
+      break;
+    case 1:
+      fSModel.updateRightLine(topLeftPos, topLeftPos.plus(dimens), false);
+      break;
+    case 2:
+      fSModel.updateTopLine(topLeftPos, topLeftPos.plus(dimens), false);
+      break;
+    case 3:
+      fSModel.updateBottomLine(topLeftPos, topLeftPos.plus(dimens), false);
+      break;
+    default:
+      console.log("sideOscillator invalid");
+  }
 }
